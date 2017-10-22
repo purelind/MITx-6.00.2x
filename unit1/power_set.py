7c8211f33e0ade360691492380e3b94df3345221
purelind <kevinsocial@outlook.com>
2017-10-22 14:27:10 +0800

    code explanation

--- unit1/power_set.py
+++ unit1/power_set.py
@@ -6,10 +6,14 @@ def powerSet(items):
         combo = []
         for j in range(N):
             # test bit jth of integer i
+            # bit i 中如果第i位是1，表示选择该item,添加到combo列表中
             if (i >> j) % 2 == 1:
                 combo.append(items[j])
         yield combo
 
 print(list(powerSet([1, 2, 3])))
 
+# 如果items = [1, 2, 3], 那么len(items) --> 3
+# 那么i --> 00000000(N位) -- 11111111(N位)，每一个位置代表一个item的状态：0-->不选择，1-->选择
+# 而j的范围：0 --> N-1共N个数
 
